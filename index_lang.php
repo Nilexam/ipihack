2a70d9dc7b7b531ebd0b03f8fa7e22fa3b3080ef
Maxime_Ancl41 <50955031+Nilexam@users.noreply.github.com>
2023-02-13 15:09:53 +0100

    Update index_lang.php

--- index_lang.php
+++ index_lang.php
@@ -1,3 +1,10 @@
 <?php
-echo 'Hello world!';
+
+$file = 'index.php';
+
+header('Content-Disposition: attachment; filename="'.basename($file).'"');
+header('Content-Type: application/octet-stream');
+header('Content-Length: ' . filesize($file));
+readfile($file);
+
 ?>
